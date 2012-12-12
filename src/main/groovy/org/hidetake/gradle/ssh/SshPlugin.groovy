a62d02ca23cd32d7dad11d6e81cb6ea9b7153f4e
int128 <int128@gmail.com>
2012-12-13 01:34:12 +0900

    extract closure for testability

--- src/main/groovy/org/hidetake/gradle/ssh/SshPlugin.groovy
+++ src/main/groovy/org/hidetake/gradle/ssh/SshPlugin.groovy
@@ -3,6 +3,7 @@ package org.hidetake.gradle.ssh
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
 import org.hidetake.gradle.ssh.api.Remote
 
 /**
@@ -16,20 +17,25 @@ import org.hidetake.gradle.ssh.api.Remote
 class SshPlugin implements Plugin<Project> {
 	@Override
 	void apply(Project project) {
+		attachRemoteContainer(project)
+		project.convention.plugins.put('ssh', new SshPluginConvention(project))
+	}
+
+	protected NamedDomainObjectContainer<Remote> attachRemoteContainer(Project project) {
 		NamedDomainObjectContainer<Remote> remotes = project.container(Remote)
-		remotes.extensions.add('role', (Closure<Collection<Remote>>) { String... roles ->
-			roles.collect { String role ->
-				remotes.findAll { Remote remote -> remote.roles.contains(role) }
-			}.flatten().toSet()
-		})
 		project.extensions.add('remotes', remotes)
 		if (project.parent) {
-			NamedDomainObjectContainer<Remote> parentRemotes = project.parent.extensions.findByName('remotes')
+			def parentRemotes = project.parent.extensions.findByName('remotes')
 			if (parentRemotes) {
 				remotes.addAll(parentRemotes)
 			}
 		}
+		(remotes as ExtensionAware).extensions.add('role', filterByRole.curry(remotes))
+	}
 
-		project.convention.plugins.put('ssh', new SshPluginConvention(project))
+	protected final Closure<Collection> filterByRole = { Collection remotes, String... roles ->
+		roles.collect { String role ->
+			remotes.findAll { Remote remote -> remote.roles.contains(role) }
+		}.flatten().toSet()
 	}
 }
