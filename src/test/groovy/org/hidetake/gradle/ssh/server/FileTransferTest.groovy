c925b6103f9919cf2130e816b2d044d72c44343d
int128 <int128@gmail.com>
2013-11-09 07:40:57 +0900

    Added tests and examples of file transfer

---
+++ src/test/groovy/org/hidetake/gradle/ssh/server/FileTransferTest.groovy
@@ -0,0 +1,105 @@
+package org.hidetake.gradle.ssh.server
+
+import org.apache.sshd.SshServer
+import org.apache.sshd.server.PasswordAuthenticator
+import org.apache.sshd.server.sftp.SftpSubsystem
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.hidetake.gradle.ssh.SshTask
+import org.hidetake.gradle.ssh.test.ServerBasedTestHelper
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Shared
+import spock.lang.Specification
+
+class FileTransferTest extends Specification {
+
+    @Shared
+    SshServer server
+
+    Project project
+
+    @Rule
+    TemporaryFolder temporaryFolder
+
+    def setupSpec() {
+        server = ServerBasedTestHelper.setUpLocalhostServer()
+        server.passwordAuthenticator = Mock(PasswordAuthenticator) {
+            _ * authenticate('someuser', 'somepassword', _) >> true
+        }
+        server.subsystemFactories = [new SftpSubsystem.Factory()]
+        server.start()
+    }
+
+    def teardownSpec() {
+        server.stop(true)
+    }
+
+
+    def setup() {
+        project = ProjectBuilder.builder().build()
+        project.with {
+            apply plugin: 'ssh'
+            ssh {
+                config(StrictHostKeyChecking: 'no')
+            }
+            remotes {
+                testServer {
+                    host = server.host
+                    port = server.port
+                    user = 'someuser'
+                    password = 'somepassword'
+                }
+            }
+        }
+    }
+
+
+    def "put a file"() {
+        given:
+        def sourceFile = temporaryFolder.newFile()
+        def destinationFile = temporaryFolder.newFile() << uuidgen()
+
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    put(sourceFile.path, destinationFile.path)
+                }
+            }
+        }
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        destinationFile.exists()
+        destinationFile.text == sourceFile.text
+    }
+
+    def "get a file"() {
+        given:
+        def sourceFile = temporaryFolder.newFile()
+        def destinationFile = temporaryFolder.newFile() << uuidgen()
+
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    get(sourceFile.path, destinationFile.path)
+                }
+            }
+        }
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        destinationFile.exists()
+        destinationFile.text == sourceFile.text
+    }
+
+
+    static uuidgen() {
+        UUID.randomUUID().toString()
+    }
+
+}
