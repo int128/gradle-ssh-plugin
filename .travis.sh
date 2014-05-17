e597350a9b7787de9418d26956600b0cc75b5532
int128 <int128@gmail.com>
2014-05-17 14:18:57 +0900

    Add generating branch list

--- .travis.sh
+++ .travis.sh
@@ -38,8 +38,11 @@ function publish_report () {
 
         [ -d ../build/reports ] && cp -a ../build/reports "$TRAVIS_BRANCH"
         [ -d ../build/docs    ] && cp -a ../build/docs    "$TRAVIS_BRANCH"
-
         git add "$TRAVIS_BRANCH"
+
+        grep "$TRAVIS_BRANCH" branch-list || echo "$TRAVIS_BRANCH" >> branch-list
+        git add branch-list
+
         git commit -q -m "Automatically updated by Travis build $TRAVIS_BUILD_NUMBER"
         git push origin gh-pages
 
