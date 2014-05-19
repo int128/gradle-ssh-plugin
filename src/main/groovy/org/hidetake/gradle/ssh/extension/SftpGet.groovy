9fc62e205a033c4a239fd1fa5850f70b360caac1
int128 <int128@gmail.com>
2014-05-19 22:40:55 +0900

    Split the file transfer extension to get and put

--- src/main/groovy/org/hidetake/gradle/ssh/extension/SftpGet.groovy
+++ src/main/groovy/org/hidetake/gradle/ssh/extension/SftpGet.groovy
@@ -6,13 +6,13 @@ import groovy.util.logging.Slf4j
 import org.hidetake.gradle.ssh.plugin.session.SessionHandler
 
 /**
- * An extension class of file transfer.
+ * An extension class to get a file or directory via SFTP.
  *
  * @author hidetake.org
  */
 @Category(SessionHandler)
 @Slf4j
-class FileTransfer {
+class SftpGet {
     /**
      * Get a file or directory from the remote host.
      *
@@ -73,56 +73,5 @@ class FileTransfer {
                 throw new RuntimeException(e)
             }
         }
-    }
-
-    /**
-     * Put a file or directory to the remote host.
-     *
-     * @param local
-     * @param remote
-     */
-    void put(File local, String remote) {
-        assert remote, 'remote path must be given'
-        assert local,  'local file must be given'
-        operations.sftp(sftpPutRecursive.curry([local], remote))
-    }
-
-    /**
-     * Put a file or directory to the remote host.
-     *
-     * @param local
-     * @param remote
-     */
-    void put(String local, String remote) {
-        assert remote, 'remote path must be given'
-        assert local,  'local path must be given'
-        operations.sftp(sftpPutRecursive.curry([new File(local)], remote))
-    }
-
-    /**
-     * Put a collection of a file or directory to the remote host.
-     *
-     * @param local
-     * @param remote
-     */
-    void put(Iterable<File> local, String remote) {
-        assert remote, 'remote path must be given'
-        assert local,  'local path must be given'
-        operations.sftp(sftpPutRecursive.curry(local, remote))
-    }
-
-    private static final sftpPutRecursive = { Iterable<File> localFiles, String remoteFile ->
-        final Closure putInternal
-        putInternal = { File localPath, String remotePath ->
-            if (localPath.directory) {
-                def remoteDir = "$remotePath/${localPath.name}"
-                mkdir(remoteDir)
-                localPath.eachFile(putInternal.rcurry(remoteDir))
-            } else {
-                putFile(localPath.path, remotePath)
-            }
-        }
-
-        localFiles.each { localFile -> putInternal(localFile, remoteFile) }
     }
 }
