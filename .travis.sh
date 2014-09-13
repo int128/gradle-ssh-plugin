9e63ff01b601d800274540f0b3b45adf1900a19c
int128 <int128@gmail.com>
2014-09-13 22:50:24 +0900

    Remove acceptance tests

--- .travis.sh
+++ .travis.sh
@@ -1,25 +1,5 @@
 #!/bin/bash -xe
 
-function acceptance_test () {
-    mkdir -p build/reports
-
-    ssh-keygen -t rsa -N '' -C '' -f ~/.ssh/id_rsa
-    ssh-keygen -t rsa -N 'pass_phrase' -C '' -f ~/.ssh/id_rsa_pass
-    cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-
-    ssh -o StrictHostKeyChecking=no \
-        -o UserKnownHostsFile=~/.ssh/known_hosts \
-        -o HostKeyAlgorithms=ssh-rsa \
-        -i ~/.ssh/id_rsa localhost true
-    ssh-keygen -H -F localhost
-
-    ./gradlew -i -s -p acceptance-test test aggressiveTest >> "build/reports/acceptance-test.log"
-
-    eval $(ssh-agent)
-    ssh-add ~/.ssh/id_rsa
-    ./gradlew -i -s -p acceptance-test testWithAgent >> "build/reports/acceptance-test.log"
-}
-
 function publish_report () {
     if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
         set +x
