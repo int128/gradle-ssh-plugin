5190414d98f1279b9fce8ab411cc3931ace037ce
int128 <int128@gmail.com>
2014-10-01 10:42:35 +0900

    Revert "Remove plugin code and fix dependencies"
    This reverts commit d1e5abffb9246640c41a16ec9c52af8a291d13f2.

---
+++ src/test/groovy/org/hidetake/gradle/ssh/server/HostKeyCheckingSpec.groovy
@@ -0,0 +1,176 @@
+package org.hidetake.gradle.ssh.server
+
+import com.jcraft.jsch.JSchException
+import groovy.util.logging.Slf4j
+import org.apache.sshd.SshServer
+import org.apache.sshd.server.CommandFactory
+import org.apache.sshd.server.PasswordAuthenticator
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskExecutionException
+import org.gradle.testfixtures.ProjectBuilder
+import org.hidetake.gradle.ssh.plugin.SshTask
+import org.hidetake.gradle.ssh.test.SshServerMock
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+import static org.hidetake.gradle.ssh.test.SshServerMock.commandWithExit
+
+@org.junit.experimental.categories.Category(ServerIntegrationTest)
+@Slf4j
+class HostKeyCheckingSpec extends Specification {
+
+    SshServer server
+    Project project
+
+    @Rule
+    TemporaryFolder temporaryFolder
+
+    def setup() {
+        server = SshServerMock.setUpLocalhostServer()
+        server.passwordAuthenticator = Mock(PasswordAuthenticator)
+        server.commandFactory = Mock(CommandFactory)
+        server.start()
+
+        project = ProjectBuilder.builder().build()
+        project.with {
+            apply plugin: 'ssh'
+            remotes {
+                testServer {
+                    host = server.host
+                    port = server.port
+                    user = 'someuser'
+                    password = 'somepassword'
+                }
+            }
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    execute 'somecommand'
+                }
+            }
+        }
+    }
+
+    def cleanup() {
+        server.stop(true)
+    }
+
+
+    def "turn off strict host key checking"() {
+        given:
+        project.with {
+            ssh {
+                knownHosts = allowAnyHosts
+            }
+        }
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * server.passwordAuthenticator.authenticate('someuser', 'somepassword', _) >> true
+        1 * server.commandFactory.createCommand('somecommand') >> commandWithExit(0)
+    }
+
+    def "turn off strict host key checking by per remote settings"() {
+        given:
+        project.with {
+            remotes {
+                testServer {
+                    knownHosts = allowAnyHosts
+                }
+            }
+        }
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * server.passwordAuthenticator.authenticate('someuser', 'somepassword', _) >> true
+        1 * server.commandFactory.createCommand('somecommand') >> commandWithExit(0)
+    }
+
+    def "strict host key checking with a valid known-hosts"() {
+        given:
+        def hostKey = HostKeyCheckingSpec.getResourceAsStream('/hostkey.pub').text
+        def knownHostsFile = temporaryFolder.newFile() << "[localhost]:${server.port} ${hostKey}"
+
+        project.with {
+            ssh {
+                knownHosts = knownHostsFile
+            }
+        }
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * server.passwordAuthenticator.authenticate('someuser', 'somepassword', _) >> true
+        1 * server.commandFactory.createCommand('somecommand') >> commandWithExit(0)
+    }
+
+    def "strict host key checking with a hashed known-hosts"() {
+        given:
+        def hostname = "[localhost]:${server.port}"
+        def salt = randomBytes(20)
+        def hash = hmacSha1(salt, hostname.getBytes())
+
+        def hostKey = HostKeyCheckingSpec.getResourceAsStream('/hostkey.pub').text
+        def knownHostsItem = "|1|${salt.encodeBase64()}|${hash.encodeBase64()} ${hostKey}"
+        def knownHostsFile = temporaryFolder.newFile() << knownHostsItem
+        log.debug(knownHostsItem)
+
+        project.with {
+            ssh {
+                knownHosts = knownHostsFile
+            }
+        }
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * server.passwordAuthenticator.authenticate('someuser', 'somepassword', _) >> true
+        1 * server.commandFactory.createCommand('somecommand') >> commandWithExit(0)
+    }
+
+    def "strict host key checking with an empty known-hosts"() {
+        given:
+        def knownHostsFile = temporaryFolder.newFile()
+
+        project.with {
+            ssh {
+                knownHosts = knownHostsFile
+            }
+        }
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        0 * server.passwordAuthenticator.authenticate('someuser', 'somepassword', _)
+        0 * server.commandFactory.createCommand('somecommand')
+
+        then:
+        TaskExecutionException e = thrown()
+        e.cause.cause instanceof JSchException
+        e.cause.cause.message.contains 'reject HostKey'
+    }
+
+    private static randomBytes(int size) {
+        def bytes = new byte[size]
+        new Random().nextBytes(bytes)
+        bytes
+    }
+
+    private static hmacSha1(byte[] salt, byte[] data) {
+        def key = new SecretKeySpec(salt, 'HmacSHA1')
+        def mac = Mac.getInstance(key.algorithm)
+        mac.init(key)
+        mac.doFinal(data)
+    }
+
+}
---
+++ src/test/groovy/org/hidetake/gradle/ssh/server/SudoCommandExecutionSpec.groovy
@@ -0,0 +1,344 @@
+package org.hidetake.gradle.ssh.server
+
+import org.apache.sshd.SshServer
+import org.apache.sshd.server.CommandFactory
+import org.apache.sshd.server.PasswordAuthenticator
+import org.codehaus.groovy.tools.Utilities
+import org.gradle.api.Project
+import org.gradle.api.tasks.TaskExecutionException
+import org.gradle.testfixtures.ProjectBuilder
+import org.hidetake.gradle.ssh.plugin.SshTask
+import org.hidetake.gradle.ssh.test.SshServerMock
+import org.hidetake.gradle.ssh.test.SshServerMock.CommandContext
+import org.hidetake.groovy.ssh.internal.operation.DefaultOperations
+import org.slf4j.Logger
+import spock.lang.Specification
+import spock.lang.Unroll
+import spock.util.mop.ConfineMetaClassChanges
+
+@org.junit.experimental.categories.Category(ServerIntegrationTest)
+class SudoCommandExecutionSpec extends Specification {
+
+    private static final NL = Utilities.eol()
+
+    SshServer server
+    Project project
+
+    def setup() {
+        server = SshServerMock.setUpLocalhostServer()
+        server.passwordAuthenticator = Mock(PasswordAuthenticator) {
+            _ * authenticate('someuser', 'somepassword', _) >> true
+        }
+
+        project = ProjectBuilder.builder().build()
+        project.with {
+            apply plugin: 'ssh'
+            ssh {
+                knownHosts = allowAnyHosts
+            }
+            remotes {
+                testServer {
+                    host = server.host
+                    port = server.port
+                    user = 'someuser'
+                    password = 'somepassword'
+                }
+            }
+        }
+    }
+
+    def cleanup() {
+        server.stop(true)
+    }
+
+    def parseSudoCommandLine(String commandline) {
+        def matcher = commandline =~ /^sudo -S -p '(.+?)' (.+)$/
+        assert matcher.matches()
+        def groups = matcher[0] as List
+        [prompt: groups[1], commandline: groups[2]]
+    }
+
+
+    def "execute commands sequentially"() {
+        given:
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    executeSudo 'somecommand1'
+                    executeSudo 'somecommand2'
+                    executeSudo 'somecommand3'
+                }
+            }
+        }
+
+        def recorder = Mock(Closure)
+        server.commandFactory = Mock(CommandFactory) {
+            createCommand(_) >> { String commandline ->
+                SshServerMock.command { CommandContext c ->
+                    def sudo = parseSudoCommandLine(commandline)
+                    recorder(sudo.commandline)
+                    c.outputStream.withWriter('UTF-8') { it << sudo.prompt << '\n' }
+                    c.exitCallback.onExit(0)
+                }
+            }
+        }
+        server.start()
+
+        when:
+        project.tasks.testTask.execute()
+
+        then: 1 * recorder.call('somecommand1')
+        then: 1 * recorder.call('somecommand2')
+        then: 1 * recorder.call('somecommand3')
+    }
+
+    def "handling authentication failure"() {
+        given:
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    executeSudo 'somecommand'
+                }
+            }
+        }
+
+        def recorder = Mock(Closure)
+        server.commandFactory = Mock(CommandFactory) {
+            createCommand(_) >> { String commandline ->
+                SshServerMock.command { CommandContext c ->
+                    def sudo = parseSudoCommandLine(commandline)
+                    recorder(sudo.commandline)
+                    c.outputStream.withWriter('UTF-8') {
+                        it << sudo.prompt
+                        it.flush()
+                        it << '\n' << 'Sorry, try again.' << '\n'
+                    }
+                    c.exitCallback.onExit(1)
+                }
+            }
+        }
+        server.start()
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * recorder.call('somecommand')
+
+        then:
+        TaskExecutionException e = thrown()
+        e.cause.message.contains('exit status -1')
+    }
+
+    def "handling command failure"() {
+        given:
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    executeSudo 'somecommand'
+                }
+            }
+        }
+
+        def recorder = Mock(Closure)
+        server.commandFactory = Mock(CommandFactory) {
+            createCommand(_) >> { String commandline ->
+                SshServerMock.command { CommandContext c ->
+                    def sudo = parseSudoCommandLine(commandline)
+                    recorder(sudo.commandline)
+                    c.outputStream.withWriter('UTF-8') { it << sudo.prompt << '\n' }
+                    c.exitCallback.onExit(1)
+                }
+            }
+        }
+        server.start()
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * recorder.call('somecommand')
+
+        then:
+        TaskExecutionException e = thrown()
+        e.cause.message.contains('exit status 1')
+    }
+
+    @Unroll
+    def "obtain a command result, #description"() {
+        given:
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    project.ext.resultActual = executeSudo 'somecommand'
+                }
+            }
+        }
+
+        def recorder = Mock(Closure)
+        server.commandFactory = Mock(CommandFactory) {
+            createCommand(_) >> { String commandline ->
+                SshServerMock.command { CommandContext c ->
+                    def sudo = parseSudoCommandLine(commandline)
+                    recorder(sudo.commandline)
+                    c.outputStream.withWriter('UTF-8') {
+                        it << sudo.prompt
+                        it.flush()
+                        it << '\n' << outputValue
+                    }
+                    c.exitCallback.onExit(0)
+                }
+            }
+        }
+        server.start()
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * recorder.call('somecommand')
+
+        then:
+        project.ext.resultActual == resultExpected
+
+        where:
+        description            | outputValue                  | resultExpected
+        'empty'                | ''                           | ''
+        'a line'               | 'some result'                | 'some result'
+        'a line with line sep' | 'some result\n'              | 'some result'
+        'lines'                | 'some result\nsecond line'   | "some result${NL}second line"
+        'lines with line sep'  | 'some result\nsecond line\n' | "some result${NL}second line"
+    }
+
+    def "obtain a command result via callback"() {
+        given:
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    executeSudo('somecommand') { result ->
+                        project.ext.resultActual = result
+                    }
+                }
+            }
+        }
+
+        def recorder = Mock(Closure)
+        server.commandFactory = Mock(CommandFactory) {
+            createCommand(_) >> { String commandline ->
+                SshServerMock.command { CommandContext c ->
+                    def sudo = parseSudoCommandLine(commandline)
+                    recorder(sudo.commandline)
+                    c.outputStream.withWriter('UTF-8') {
+                        it << sudo.prompt
+                        it.flush()
+                        it << '\n' << 'something output'
+                    }
+                    c.exitCallback.onExit(0)
+                }
+            }
+        }
+        server.start()
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * recorder.call('somecommand')
+
+        then:
+        project.ext.resultActual == 'something output'
+    }
+
+    def "obtain a command result via callback with settings"() {
+        given:
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    executeSudo('somecommand', pty: true) { result ->
+                        project.ext.resultActual = result
+                    }
+                }
+            }
+        }
+
+        def recorder = Mock(Closure)
+        server.commandFactory = Mock(CommandFactory) {
+            createCommand(_) >> { String commandline ->
+                SshServerMock.command { CommandContext c ->
+                    def sudo = parseSudoCommandLine(commandline)
+                    recorder(sudo.commandline)
+                    c.outputStream.withWriter('UTF-8') {
+                        it << sudo.prompt
+                        it.flush()
+                        it << '\n' << 'something output'
+                    }
+                    c.exitCallback.onExit(0)
+                }
+            }
+        }
+        server.start()
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * recorder.call('somecommand')
+
+        then:
+        project.ext.resultActual == 'something output'
+    }
+
+    @Unroll
+    @ConfineMetaClassChanges(DefaultOperations)
+    def "logging, #description"() {
+        given:
+        def logger = Mock(Logger) {
+            isInfoEnabled() >> true
+        }
+        DefaultOperations.metaClass.static.getLog = { -> logger }
+
+        project.with {
+            task(type: SshTask, 'testTask') {
+                session(remotes.testServer) {
+                    executeSudo 'somecommand'
+                }
+            }
+        }
+
+        def recorder = Mock(Closure)
+        server.commandFactory = Mock(CommandFactory) {
+            createCommand(_) >> { String commandline ->
+                SshServerMock.command { CommandContext c ->
+                    def sudo = parseSudoCommandLine(commandline)
+                    recorder(sudo.commandline)
+                    c.outputStream.withWriter('UTF-8') {
+                        it << sudo.prompt
+                        it.flush()
+                        it << '\n' << outputValue
+                    }
+                    c.exitCallback.onExit(0)
+                }
+            }
+        }
+        server.start()
+
+        when:
+        project.tasks.testTask.execute()
+
+        then:
+        1 * recorder.call('somecommand')
+
+        then:
+        logMessages.each {
+            1 * logger.info(it)
+        }
+
+        where:
+        description            | outputValue                  | logMessages
+        'a line'               | 'some result'                | ['some result']
+        'a line with line sep' | 'some result\n'              | ['some result']
+        'lines'                | 'some result\nsecond line'   | ['some result', 'second line']
+        'lines with line sep'  | 'some result\nsecond line\n' | ['some result', 'second line']
+    }
+
+}
