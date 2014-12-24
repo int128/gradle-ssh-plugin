b445f29dea24284d29650f4f79d3bf72c858e996
int128 <int128@gmail.com>
2014-12-24 18:12:27 +0900

    Add acceptance test on Gradle 1.x

--- acceptance-test/run-test.sh
+++ acceptance-test/run-test.sh
@@ -1,22 +1,31 @@
 #!/bin/bash -xe
 #
+# Run acceptance tests on Travis CI.
+#
+# Environment Variables:
+# - GRADLE_VERSION: Gradle version (mandatory)
+# - version: Product version (SNAPSHOT version if not given)
+#
 # Requirements:
 # - sshd must be running
 # - sudo must be enabled without password
 # - java must be installed
 
-# move to acceptance test directory
 cd $(dirname $0)
+test "$GRADLE_VERSION"
 
-# determine Gradle path
-[ -x ../gradlew ] && gradle=../gradlew
-[ -x ./gradle/bin/gradle ] && gradle=./gradle/bin/gradle
+# install Gradle
+curl -LO "https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip"
+unzip -q "gradle-${GRADLE_VERSION}-bin.zip"
+chmod +x "gradle-${GRADLE_VERSION}/bin/gradle"
+ln -vsf  "gradle-${GRADLE_VERSION}/bin/gradle"
+./gradle --version
 
 # enable public key authentication
-mkdir -m 700 -p -v                    $HOME/.ssh
-ssh-keygen -t rsa -N ''            -f $HOME/.ssh/id_rsa
-ssh-keygen -t rsa -N 'pass_phrase' -f $HOME/.ssh/id_rsa_pass
-cat $HOME/.ssh/id_rsa.pub           > $HOME/.ssh/authorized_keys
+mkdir -m 700 -p -v $HOME/.ssh
+[ -f $HOME/.ssh/id_rsa ]          || ssh-keygen -t rsa -N ''            -f $HOME/.ssh/id_rsa
+[ -f $HOME/.ssh/id_rsa_pass ]     || ssh-keygen -t rsa -N 'pass_phrase' -f $HOME/.ssh/id_rsa_pass
+[ -f $HOME/.ssh/authorized_keys ] || cat $HOME/.ssh/id_rsa.pub           > $HOME/.ssh/authorized_keys
 
 # generate a known hosts file
 ssh -o StrictHostKeyChecking=no \
@@ -27,10 +36,10 @@ ssh -o StrictHostKeyChecking=no \
 ssh-keygen -H -F localhost
 
 # run tests
-"$gradle" -i -s -Pversion="${version:-SNAPSHOT}" test testWithSideEffect
+./gradle -s -Pversion="${version:-SNAPSHOT}" test testWithSideEffect
 
 # run tests with ssh-agent
 eval $(ssh-agent)
 ssh-add $HOME/.ssh/id_rsa
-"$gradle" -i -s -Pversion="${version:-SNAPSHOT}" testWithSshAgent
+./gradle -s -Pversion="${version:-SNAPSHOT}" testWithSshAgent
 ssh-agent -k
