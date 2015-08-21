42834a318f16720496fc60aeb4fb287709216e58
int128 <int128@gmail.com>
2015-08-21 22:49:24 +0900

    Refactor releasing document

---
+++ core/src/docs/shell/release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+test "$TRAVIS_BRANCH" = "master"
+
+PR_SOURCE="core/build/asciidoc/html5"
+PR_REPO="gradle-ssh-plugin/gradle-ssh-plugin.github.io"
+PR_BRANCH="travis-${TRAVIS_BUILD_NUMBER}"
+PR_MESSAGE="Release document"
+
+test -d "$PR_SOURCE"
+
+hub clone "$PR_REPO" _
+cd _
+hub checkout -b "$PR_BRANCH"
+hub rm -r docs
+cp -a "../$PR_SOURCE" docs
+hub add .
+hub commit -m "Document from $TRAVIS_REPO_SLUG:$TRAVIS_BRANCH $TRAVIS_COMMIT_RANGE"
+hub push origin "$PR_BRANCH"
+hub pull-request -m "Document from $TRAVIS_REPO_SLUG:$TRAVIS_BRANCH"
+cd ..
