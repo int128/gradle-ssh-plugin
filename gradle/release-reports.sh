01347309a0f9279b5a46ae7d47ea0e8f1cc44d39
int128 <int128@gmail.com>
2015-09-05 19:18:59 +0900

    Fix release if some tests were error

--- gradle/release-reports.sh
+++ gradle/release-reports.sh
@@ -4,10 +4,13 @@ hub clone gradle-ssh-plugin/reports _
 cd _
 
 hub rm -fr *
-cp -av ../cli/build/reports cli
-cp -av ../core/build/reports core
-cp -av ../os-integration-test/build/reports os-integration-test
-cp -av ../server-integration-test/build/reports server-integration-test
+for component in cli core os-integration-test server-integration-test
+do
+    if [ -d ../$component/build/reports/tests ]
+    then
+        cp -av ../$component/build/reports/tests $component
+    fi
+done
 
 hub add .
 hub commit -m "Build reports from $TRAVIS_BRANCH of $TRAVIS_REPO_SLUG"
