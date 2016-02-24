c98857392747ebe79372294673a830e705932ca0
int128 <int128@gmail.com>
2016-02-24 22:19:33 +0900

    Specify Gradle version on acceptance-test.sh

--- gradle/acceptance-test.sh
+++ gradle/acceptance-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -xe
 
-./gradlew --version
-./gradlew -p acceptance-test -s test
+GRADLE_VERSION="$1"
+if [ "$GRADLE_VERSION" ]; then
+  sed -i -e "s,gradle-[0-9.]*-,gradle-${GRADLE_VERSION}-,g" gradle/wrapper/gradle-wrapper.properties
+fi
 
-sed -i -e 's,gradle-[0-9.]*-,gradle-1.12-,g' gradle/wrapper/gradle-wrapper.properties
 ./gradlew --version
 ./gradlew -p acceptance-test -s test
 
 git reset --hard
-./gradlew --version
