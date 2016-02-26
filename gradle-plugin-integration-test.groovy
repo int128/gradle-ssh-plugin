474d20e7a171397a2fa86e48d319b8bb880b3b87
int128 <int128@gmail.com>
2016-02-26 22:27:36 +0900

    Run plugin integration test in parallel

--- gradle-plugin-integration-test.groovy
+++ gradle-plugin-integration-test.groovy
@@ -10,45 +10,63 @@ ssh.remotes {
         user = System.getenv('INTEGRATION_TEST_SSH_USER')
         identity = new File("${System.getProperty('user.home')}/.ssh/id_ec2")
     }
-    tester {
+    tester1 {
+        role 'testers'
         host = System.getenv('INTEGRATION_TEST_SSH_HOST')
         user = "user${randomInt()}"
-        password = "password${randomInt()}"
+        identity = new File("${System.getProperty('user.home')}/.ssh/id_ec2")
+    }
+    tester2 {
+        role 'testers'
+        host = System.getenv('INTEGRATION_TEST_SSH_HOST')
+        user = "user${randomInt()}"
+        identity = new File("${System.getProperty('user.home')}/.ssh/id_ec2")
     }
 }
 
 ssh.run {
     session(ssh.remotes.ec2) {
         execute 'sudo yum install -y git', pty: true
-        execute "sudo useradd -m ${ssh.remotes.tester.user}", pty: true
-        execute "sudo passwd $ssh.remotes.tester.user", pty: true, interaction: {
-            when(partial: ~/.+[Pp]assword: */) {
-                standardInput << ssh.remotes.tester.password << '\n'
-            }
-        }
     }
 }
 
 try {
     ssh.run {
-        session(ssh.remotes.tester) {
-            execute 'mkdir -m 700 -p -v .ssh'
-            execute 'ssh-keygen -t rsa -N "" -f .ssh/id_rsa'
-            execute 'cat .ssh/id_rsa.pub > .ssh/authorized_keys'
-            execute 'chmod -v 600 .ssh/authorized_keys'
-            execute 'ssh -o StrictHostKeyChecking=no -o HostKeyAlgorithms=ssh-rsa -i .ssh/id_rsa localhost id'
-            execute 'ssh-keygen -H -F localhost'
-
+        session(ssh.remotes.ec2) {
+            ssh.remotes.role('testers').each { tester ->
+                execute """
+sudo useradd -m $tester.user
+sudo -i -u $tester.user mkdir -m 700 .ssh
+sudo -i -u $tester.user tee .ssh/authorized_keys < .ssh/authorized_keys
+sudo -i -u $tester.user chmod 600 .ssh/authorized_keys
+""", pty: true
+            }
+        }
+        session(ssh.remotes.role('testers')) {
+            executeBackground '''
+ssh-keygen -t rsa -N "" -f .ssh/id_rsa
+cat .ssh/id_rsa.pub >> .ssh/authorized_keys
+ssh -o StrictHostKeyChecking=no -o HostKeyAlgorithms=ssh-rsa -i .ssh/id_rsa localhost id
+ssh-keygen -H -F localhost
+git clone --depth 1 https://github.com/int128/gradle-ssh-plugin
+'''
             put from: "${System.getProperty('user.home')}/.m2", into: '.'
-            execute 'git clone --depth 1 https://github.com/int128/gradle-ssh-plugin'
-            execute 'cd gradle-ssh-plugin && TERM=dumb ./gradle/acceptance-test.sh', pty: true
-            execute 'cd gradle-ssh-plugin && TERM=dumb ./gradle/acceptance-test.sh 1.12', pty: true
+        }
+    }
+    ssh.run {
+        session(ssh.remotes.tester1) {
+            executeBackground 'cd gradle-ssh-plugin && TERM=dumb ./gradle/acceptance-test.sh 1.12', pty: true
+        }
+        session(ssh.remotes.tester2) {
+            executeBackground 'cd gradle-ssh-plugin && TERM=dumb ./gradle/acceptance-test.sh', pty: true
         }
     }
 } finally {
     ssh.run {
         session(ssh.remotes.ec2) {
-            execute "sudo userdel -r ${ssh.remotes.tester.user}", pty: true
+            ssh.remotes.role('testers').each { tester ->
+                execute "sudo userdel -r $tester.user", pty: true
+            }
         }
     }
 }
