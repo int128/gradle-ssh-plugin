dantonini/groovy-ssh

Refactor settings hierarchy

core/src/main/groovy/org/hidetake/groovy/ssh/extension/Shell.groovy
@@ -1,6 +1,6 @@
 package org.hidetake.groovy.ssh.extension
 
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
+import org.hidetake.groovy.ssh.operation.ShellSettings
 import org.hidetake.groovy.ssh.session.BadExitStatusException
 import org.hidetake.groovy.ssh.session.SessionExtension
 
@@ -20,7 +20,7 @@ trait Shell implements SessionExtension {
     void shell(HashMap map) {
         assert map != null, 'map must not be null'
 
-        def settings = operationSettings + new OperationSettings(map)
+        def settings = globalSettings + new ShellSettings(map)
         def operation = operations.shell(settings)
 
         def exitStatus = operation.startSync()

core/src/main/groovy/org/hidetake/groovy/ssh/operation/DefaultOperations.groovy
@@ -3,7 +3,6 @@ package org.hidetake.groovy.ssh.operation
 import groovy.util.logging.Slf4j
 import org.hidetake.groovy.ssh.connection.Connection
 import org.hidetake.groovy.ssh.core.Remote
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.extension.settings.LocalPortForwardSettings
 import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 
@@ -33,13 +32,13 @@ class DefaultOperations implements Operations {
     }
 
     @Override
-    Operation shell(OperationSettings settings) {
+    Operation shell(ShellSettings settings) {
         log.debug("Executing shell on $remote.name: $settings")
         new Shell(connection, settings)
     }
 
     @Override
-    Operation command(OperationSettings settings, String commandLine) {
+    Operation command(CommandSettings settings, String commandLine) {
         log.debug("Executing command on $remote.name: $commandLine: $settings")
         new Command(connection, settings, commandLine)
     }

core/src/main/groovy/org/hidetake/groovy/ssh/operation/DryRunOperations.groovy
@@ -2,7 +2,6 @@ package org.hidetake.groovy.ssh.operation
 
 import groovy.util.logging.Slf4j
 import org.hidetake.groovy.ssh.core.Remote
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.extension.settings.LocalPortForwardSettings
 import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 
@@ -21,13 +20,13 @@ class DryRunOperations implements Operations {
     }
 
     @Override
-    Operation shell(OperationSettings settings) {
+    Operation shell(ShellSettings settings) {
         log.info("Executing shell on $remote")
         new DryRunOperation()
     }
 
     @Override
-    Operation command(OperationSettings settings, String commandLine) {
+    Operation command(CommandSettings settings, String commandLine) {
         log.info("Executing command on $remote: $commandLine")
         new DryRunOperation()
     }

core/src/main/groovy/org/hidetake/groovy/ssh/operation/Operations.groovy
@@ -1,7 +1,6 @@
 package org.hidetake.groovy.ssh.operation
 
 import org.hidetake.groovy.ssh.core.Remote
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.extension.settings.LocalPortForwardSettings
 import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 
@@ -13,9 +12,9 @@ import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 interface Operations {
     Remote getRemote()
 
-    Operation shell(OperationSettings settings)
+    Operation shell(ShellSettings settings)
 
-    Operation command(OperationSettings settings, String commandLine)
+    Operation command(CommandSettings settings, String commandLine)
 
     int forwardLocalPort(LocalPortForwardSettings settings)
 

core/src/main/groovy/org/hidetake/groovy/ssh/operation/Shell.groovy
@@ -4,7 +4,6 @@ import com.jcraft.jsch.ChannelShell
 import groovy.util.logging.Slf4j
 import org.hidetake.groovy.ssh.connection.Connection
 import org.hidetake.groovy.ssh.core.settings.LoggingMethod
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.interaction.Interaction
 
 /**
@@ -19,7 +18,7 @@ class Shell implements Operation {
     private final OutputStream standardInput
     private final LineOutputStream standardOutput
 
-    def Shell(Connection connection1, OperationSettings settings) {
+    def Shell(Connection connection1, ShellSettings settings) {
         connection = connection1
         channel = connection.createShellChannel()
         standardInput = channel.outputStream

int128/gradle-ssh-plugin

core/src/main/groovy/org/hidetake/groovy/ssh/extension/Shell.groovy
@@ -1,6 +1,6 @@
 package org.hidetake.groovy.ssh.extension
 
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
+import org.hidetake.groovy.ssh.operation.CommandSettings
 import org.hidetake.groovy.ssh.session.BadExitStatusException
 import org.hidetake.groovy.ssh.session.SessionExtension
 
@@ -20,7 +20,7 @@ trait Shell implements SessionExtension {
     void shell(HashMap map) {
         assert map != null, 'map must not be null'
 
-        def settings = operationSettings + new OperationSettings(map)
+        def settings = globalSettings + new CommandSettings(map)
         def operation = operations.shell(settings)
 
         def exitStatus = operation.startSync()

core/src/main/groovy/org/hidetake/groovy/ssh/operation/DefaultOperations.groovy
@@ -3,7 +3,6 @@ package org.hidetake.groovy.ssh.operation
 import groovy.util.logging.Slf4j
 import org.hidetake.groovy.ssh.connection.Connection
 import org.hidetake.groovy.ssh.core.Remote
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.extension.settings.LocalPortForwardSettings
 import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 
@@ -33,13 +32,13 @@ class DefaultOperations implements Operations {
     }
 
     @Override
-    Operation shell(OperationSettings settings) {
+    Operation shell(CommandSettings settings) {
         log.debug("Executing shell on $remote.name: $settings")
         new Shell(connection, settings)
     }
 
     @Override
-    Operation command(OperationSettings settings, String commandLine) {
+    Operation command(CommandSettings settings, String commandLine) {
         log.debug("Executing command on $remote.name: $commandLine: $settings")
         new Command(connection, settings, commandLine)
     }

core/src/main/groovy/org/hidetake/groovy/ssh/operation/DryRunOperations.groovy
@@ -2,7 +2,6 @@ package org.hidetake.groovy.ssh.operation
 
 import groovy.util.logging.Slf4j
 import org.hidetake.groovy.ssh.core.Remote
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.extension.settings.LocalPortForwardSettings
 import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 
@@ -21,13 +20,13 @@ class DryRunOperations implements Operations {
     }
 
     @Override
-    Operation shell(OperationSettings settings) {
+    Operation shell(CommandSettings settings) {
         log.info("Executing shell on $remote")
         new DryRunOperation()
     }
 
     @Override
-    Operation command(OperationSettings settings, String commandLine) {
+    Operation command(CommandSettings settings, String commandLine) {
         log.info("Executing command on $remote: $commandLine")
         new DryRunOperation()
     }

core/src/main/groovy/org/hidetake/groovy/ssh/operation/Operations.groovy
@@ -1,7 +1,6 @@
 package org.hidetake.groovy.ssh.operation
 
 import org.hidetake.groovy.ssh.core.Remote
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.extension.settings.LocalPortForwardSettings
 import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 
@@ -13,9 +12,9 @@ import org.hidetake.groovy.ssh.extension.settings.RemotePortForwardSettings
 interface Operations {
     Remote getRemote()
 
-    Operation shell(OperationSettings settings)
+    Operation shell(CommandSettings settings)
 
-    Operation command(OperationSettings settings, String commandLine)
+    Operation command(CommandSettings settings, String commandLine)
 
     int forwardLocalPort(LocalPortForwardSettings settings)
 

core/src/main/groovy/org/hidetake/groovy/ssh/operation/Shell.groovy
@@ -4,7 +4,6 @@ import com.jcraft.jsch.ChannelShell
 import groovy.util.logging.Slf4j
 import org.hidetake.groovy.ssh.connection.Connection
 import org.hidetake.groovy.ssh.core.settings.LoggingMethod
-import org.hidetake.groovy.ssh.core.settings.OperationSettings
 import org.hidetake.groovy.ssh.interaction.Interaction
 
 /**
@@ -19,7 +18,7 @@ class Shell implements Operation {
     private final OutputStream standardInput
     private final LineOutputStream standardOutput
 
-    def Shell(Connection connection1, OperationSettings settings) {
+    def Shell(Connection connection1, CommandSettings settings) {
         connection = connection1
         channel = connection.createShellChannel()
         standardInput = channel.outputStream