b06e633684611e8641dbe304c27b60cf0288c949
Hidetake Iwata <int128@gmail.com>
2016-08-14 22:51:23 +0900

    Add release note URL on commit for the plugin

--- plugin-integration/create-branch-for-release.sh
+++ plugin-integration/create-branch-for-release.sh
@@ -6,5 +6,5 @@ git reset --hard
 git checkout -b "groovy-ssh-$CIRCLE_TAG"
 sed -i -e "s,groovy-ssh:[0-9.]*,groovy-ssh:${CIRCLE_TAG:-SNAPSHOT},g" core/build.gradle
 git add .
-git commit -m "Groovy SSH $CIRCLE_TAG"
+git commit -m "Groovy SSH $CIRCLE_TAG" -m "https://github.com/int128/groovy-ssh/releases/tag/$CIRCLE_TAG"
 git push origin
