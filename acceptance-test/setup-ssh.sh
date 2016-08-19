916957c4f2946912b5be36dc4b66ed570cbd524f
Hidetake Iwata <int128@gmail.com>
2016-08-19 20:32:36 +0900

    Move user key and host key

--- acceptance-test/setup-ssh.sh
+++ acceptance-test/setup-ssh.sh
@@ -2,15 +2,23 @@
 
 BUILD_DIR="$(dirname $0)/fixture/build"
 
-mkdir -p "$BUILD_DIR"
-rm -f "$BUILD_DIR/id_rsa"
-rm -f "$BUILD_DIR/id_rsa.pub"
-rm -f "$BUILD_DIR/known_hosts"
+rm -fr "$BUILD_DIR/.ssh"
+mkdir -p "$BUILD_DIR/.ssh"
 
-ssh-keygen -t rsa -N '' -f "$BUILD_DIR/id_rsa"
+ssh-keygen -t rsa -N '' -f "$BUILD_DIR/.ssh/id_rsa"
 
-docker run -d -e authorized_key="$(cat "$BUILD_DIR/id_rsa.pub")" -p 8022:22 int128/integration-test-box:latest
+docker run -d \
+  --name gradle-ssh-plugin-integration-test-box \
+  -e authorized_key="$(cat "$BUILD_DIR/.ssh/id_rsa.pub")" \
+  -p 8022:22 \
+  int128/integration-test-box:latest
 sleep 1
 
-ssh -o HostKeyAlgorithms=ssh-rsa -o UserKnownHostsFile="$BUILD_DIR/known_hosts" -o StrictHostKeyChecking=no -i "$BUILD_DIR/id_rsa" -p 8022 tester@sandbox.127.0.0.1.xip.io id
-ssh-keygen -F '[sandbox.127.0.0.1.xip.io]:8022' -f "$BUILD_DIR/known_hosts"
+ssh -o HostKeyAlgorithms=ssh-rsa \
+  -o UserKnownHostsFile="$BUILD_DIR/.ssh/known_hosts" \
+  -o StrictHostKeyChecking=no \
+  -i "$BUILD_DIR/.ssh/id_rsa" \
+  -p 8022 \
+  tester@sandbox.127.0.0.1.xip.io id
+
+ssh-keygen -F '[sandbox.127.0.0.1.xip.io]:8022' -f "$BUILD_DIR/.ssh/known_hosts"
