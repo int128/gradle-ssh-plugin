5a127ab5eeb083ba5a5003c0ee36ecaec3886912
Hidetake Iwata <int128@gmail.com>
2016-08-26 12:48:30 +0900

    Add tests for addHostKey and executeScript

--- acceptance-test/setup-ssh.sh
+++ acceptance-test/setup-ssh.sh
@@ -12,13 +12,3 @@ docker run -d \
   -e authorized_key="$(cat "$BUILD_DIR/.ssh/id_rsa.pub")" \
   -p 8022:22 \
   int128/integration-test-box:latest
-sleep 1
-
-ssh -o HostKeyAlgorithms=ssh-rsa \
-  -o UserKnownHostsFile="$BUILD_DIR/.ssh/known_hosts" \
-  -o StrictHostKeyChecking=no \
-  -i "$BUILD_DIR/.ssh/id_rsa" \
-  -p 8022 \
-  tester@sandbox.127.0.0.1.xip.io id
-
-ssh-keygen -F '[sandbox.127.0.0.1.xip.io]:8022' -f "$BUILD_DIR/.ssh/known_hosts"
