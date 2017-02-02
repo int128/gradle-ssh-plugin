6a4fb3950a7309e7541fe2514b55ce6766bf821e
Hidetake Iwata <int128@gmail.com>
2017-02-02 13:05:28 +0900

    Fix os-integration-test can be run on localhost

--- os-integration-test/setup-ssh.sh
+++ os-integration-test/setup-ssh.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -xe
 
+test "$DOCKER_SSH_PORT"
+test "$DOCKER_SSH_USER"
+
 BUILD_DIR="$(dirname $0)/build"
 
-rm -f "$BUILD_DIR/.ssh"
+rm -fr "$BUILD_DIR/.ssh"
 mkdir -p "$BUILD_DIR/.ssh"
 
 # generate private key
@@ -21,7 +24,7 @@ docker run -d \
   --name groovy-ssh-integration-test-box \
   --link groovy-ssh-integration-test-internal-box \
   -e authorized_key="$(cat "$BUILD_DIR/.ssh/id_rsa.pub")" \
-  -p 8022:22 \
+  -p "$DOCKER_SSH_PORT:22" \
   int128/integration-test-box:latest
 
 sleep 1
@@ -31,7 +34,7 @@ ssh -o HostKeyAlgorithms=ecdsa-sha2-nistp256 \
   -o UserKnownHostsFile="$BUILD_DIR/.ssh/known_hosts_openssh" \
   -o StrictHostKeyChecking=no \
   -i "$BUILD_DIR/.ssh/id_rsa" \
-  -p 8022 \
-  tester@sandbox.127.0.0.1.xip.io id
+  -p "$DOCKER_SSH_PORT" \
+  "$DOCKER_SSH_USER@localhost" id
 
-ssh-keygen -F '[sandbox.127.0.0.1.xip.io]:8022' -f "$BUILD_DIR/.ssh/known_hosts_openssh"
+ssh-keygen -F "[localhost]:$DOCKER_SSH_PORT" -f "$BUILD_DIR/.ssh/known_hosts_openssh"
