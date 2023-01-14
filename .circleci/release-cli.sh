a11eaae0f0c93ecbeea4096203011a6ad13f5639
Hidetake Iwata <int128@gmail.com>
2023-01-14 17:59:19 +0900

    Build on GitHub Actions (#295)
    * Build on GitHub Actions
    
    * Add
    
    * Add release

--- .circleci/release-cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-./gradlew shadowJar
-
-ghr -u int128 -r groovy-ssh \
-  -n "$CIRCLE_TAG" \
-  -b "Released on $(date +%Y-%m-%d)" \
-  "$CIRCLE_TAG" \
-  cli/build/libs/gssh.jar
--- .circleci/release-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-./gradlew asciidoctor
-
-ghcp -u gradle-ssh-plugin -r docs -b gh-pages \
-  -m "Published by $CIRCLE_BUILD_URL" \
-  -C docs/build/asciidoc/html5 \
-  .
